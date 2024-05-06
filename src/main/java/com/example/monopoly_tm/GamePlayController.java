0226ba3d7136c3d221cac5e06672d73d713be882
Tommy <manningtw@oldcolony.info>
2024-05-06 08:45:17 -0400

    whoops

--- src/main/java/com/example/monopoly_tm/GamePlayController.java
+++ src/main/java/com/example/monopoly_tm/GamePlayController.java
@@ -21,19 +21,21 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import javafx.stage.*;
 import javafx.util.Duration;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.*;
 
+import java.awt.Desktop;
+import java.net.URI;
 
 public class GamePlayController implements Initializable
 {
@@ -56,7 +58,7 @@ public class GamePlayController implements Initializable
             jail_Pane, property_functions_pane, turn_switch_pane;
 
     @FXML
-    AnchorPane dimming_ap;
+    AnchorPane dimming_ap, anchor_ap;
     @FXML
     Label current_player_info_LBL, current_player_name_LBL,
         current_player_properties_LBL, player_land_space_LBL;
@@ -92,6 +94,7 @@ public class GamePlayController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        // gets the file of the JSON file to read all the cell's information from
         File jsonFile = new File("src/main/resources/com/example/monopoly_tm/Tiles.json");
         JSONParser parser = new JSONParser();
         try
@@ -103,10 +106,14 @@ public class GamePlayController implements Initializable
             {
                 JSONObject currLineItems = (JSONObject) x;
 
+                // checks for kind of cell by checking the length
+                // of each array stored in the JSON file
                 switch (currLineItems.size())
                 {
+
                     case 2 ->
                     {
+                        // a standard cell without any way to buy or own it
                         String name = (String) currLineItems.get("name");
                         String type = (String) currLineItems.get("type");
                         cellList.add(new Cell(type, name));
@@ -114,6 +121,7 @@ public class GamePlayController implements Initializable
 
                     case 3 ->
                     {
+                        // a cell like the railroads or utilities
                         String name = (String) currLineItems.get("name");
                         String type = (String) currLineItems.get("type");
 
@@ -123,6 +131,10 @@ public class GamePlayController implements Initializable
 
                     default ->
                     {
+                        /*
+                        * a property cell which is made as a property object
+                        * which is a child of cell
+                        * and adds the new property object to the cell array list*/
                         String name = (String) currLineItems.get("name");
                         String type = (String) currLineItems.get("type");
 
@@ -137,14 +149,17 @@ public class GamePlayController implements Initializable
 
                 }
             }
+            // gets all the images for the die and adds them to an array
             setDieLists();
         }
         catch(Exception ex){
             throw new RuntimeException(ex);
         }
 
-        changePlayer(currentPlayer);
+        // set the game to start on the first play's turn
+        changeGUIPlayerText(currentPlayer);
 
+        // adds every standard cell that you pass through without jail
         paneList = new Pane[]{go_Pane, Cell_1_pane, Cell_2_pane, Cell_3_pane, Cell_4_pane, Cell_5_pane, Cell_6_pane, Cell_7_pane,
                 Cell_8_pane, Cell_9_pane, jail_visit, Cell_11_pane, Cell_12_pane, Cell_13_pane, Cell_14_pane,
                 Cell_15_pane, Cell_16_pane, Cell_17_pane, Cell_18_pane, Cell_19_pane, free_parking,
@@ -153,19 +168,26 @@ public class GamePlayController implements Initializable
                 Cell_35_pane, Cell_36_pane, Cell_37_pane, Cell_38_pane, Cell_39_pane};
 
 
-
+        // make the image of the player pieces show up on go
         makePlayerPieces(playerList.size());
 
+        // sets the table within the property functions pane
+        // will be empty at the start, but it fills as the game goes
         setPropertiesTable();
     }
 
 
     //region turn start and end
-    private void changePlayer(Players currentPlayer)
+
+    /* Change Player
+    * this method changes the text on the GUI
+    * to reflect the play whose turn it currently is
+    * then resets boolean has moved to false so the next player can move
+    * and enables the roll button so the player is able to roll the die
+    */
+    private void changeGUIPlayerText(Players currentPlayer)
     {
         int currentPlayerIndex = playerList.indexOf(currentPlayer);
-
-
 
         String name = playerList.get(currentPlayerIndex).getName();
         int balance = playerList.get(currentPlayerIndex).getBalance();
@@ -180,6 +202,16 @@ public class GamePlayController implements Initializable
         roll_btn.setDisable(false);
     }
 
+    /* End Turn
+    * checks for if the player has moved and not rolled doubles
+    * if true then it updates the current player to be whose ever turn is next
+    * it does this by adding 1 to the index of player list if the index exceeds the length
+    * the player at index of 0 is selected
+    *
+    * if the player has not moved or did roll doubles then this method will
+    * make an alert pop up with the information that is stopping the player
+    * from ending their turn
+    */
     public void endTurn(ActionEvent ignoredE)
     {
         if(hasMoved && !doubles)
@@ -191,7 +223,7 @@ public class GamePlayController implements Initializable
                     ? playerList.get(0)
                     : playerList.get(indexOfCurrentPlayer + 1);
 
-            changePlayer(currentPlayer);
+            changeGUIPlayerText(currentPlayer);
         }
         else
         {
@@ -204,7 +236,11 @@ public class GamePlayController implements Initializable
 
     //region startOfGame
 
-    private void dynamicPlayPieces(double playerPieceSize, int numOfPieces)
+    /* Dynamic Player Pieces
+    * this method sets the layout for the player pieces to space evenly in the start cell
+    * this version of this method is only called at the start of the game
+    */
+    private void dynamicPlayerPieces(double playerPieceSize, int numOfPieces)
     {
         int rowCount = (int) Math.ceil((Math.sqrt(numOfPieces)));
         int colCount = (int) Math.ceil((double) numOfPieces / rowCount);
@@ -231,8 +267,18 @@ public class GamePlayController implements Initializable
             }
     }
 
-    private void dynamicPlayPieces(double playerPieceSize,
-                                   ArrayList<Rectangle> pieces, Pane selectedPane)
+    /* Dynamic Player Pieces
+    * this method will take a cell that the player is moving too
+    * and check how many players are on that cell
+    * clears anything child of the cell that will be moved too
+    * and creates/recreates the piece moving to the cell
+    * and rearranges the old and new player pieces
+    * then rearranges the player pieces in the cell
+    * to have them space evenly and stay within the cell
+    */
+
+    private void dynamicPlayerPieces(double playerPieceSize,
+                                     ArrayList<Rectangle> pieces, Pane selectedPane)
     {
         int numOfPieces = pieces.size();
 
@@ -261,6 +307,12 @@ public class GamePlayController implements Initializable
             }
     }
 
+    /* Make Player Pieces
+    * checks how many plays are playing the game
+    * and changes the size of the player pieces
+    * depending on the player count
+    * then calls dynamicPlayerPiece, which uses just the start pane
+    */
     void makePlayerPieces(int playerCount)
     {
         double playerPieceSize;
@@ -274,9 +326,13 @@ public class GamePlayController implements Initializable
             case 7 ->  playerPieceSize = 25;
             default ->  playerPieceSize = 20;
         }
-        dynamicPlayPieces(playerPieceSize, playerCount);
+        dynamicPlayerPieces(playerPieceSize, playerCount);
     }
 
+    /* Show Game Board
+    * makes a new stage that will take the full screen
+    * this new stage will show the playable game
+    */
     public static void showGameBoard() throws IOException
     {
 //        gets rid of the top bar
@@ -326,6 +382,14 @@ public class GamePlayController implements Initializable
 
     //region dice
 
+    /* Roll Dice
+    * sets the roll button to disabled to prevent a bug
+    * then checks if the player has moved
+    * or if the player has rolled doubles
+    * if either are true it will begin
+    * the rolling animation once completed the animation
+    * the updateDieImages method
+    */
     public void rollDice(ActionEvent ignoredAe)
     {
         roll_btn.setDisable(true);
@@ -339,9 +403,18 @@ public class GamePlayController implements Initializable
         }
     }
 
+    /* Update Die Images
+    * stores the number that the two die rolls
+    * in two int variables and prints out what is rolled
+    * then adds the two values together to find how much
+    * the player will move, then checks if the two sides
+    * are of equal value to set the doubles boolean
+    * then checks if doubles is true and enables the roll button
+    * and frees you if you were in jail
+    * then checks if the player is not in jail
+    * if true the player forward the sum amount*/
     private void updateDieImages()
     {
-        int test = 4;
         int currLeftDieValue = imageToInt(left_stack);
         int currRightDieValue = imageToInt(right_stack);
         System.out.println("Die one rolled: " + currLeftDieValue +"\nDie two rolled: " + currRightDieValue);
@@ -350,19 +423,28 @@ public class GamePlayController implements Initializable
         System.out.println(currentPlayer.getName() + " Moves Forward " + diceSum + " Spaces");
 
         hasMoved = true;
-        diceSum = test;
-        /*doubles = currRightDieValue == currLeftDieValue;
+        doubles = currRightDieValue == currLeftDieValue;
 
         if (doubles)
         {
             roll_btn.setDisable(false);
             currentPlayer.setInJail(false);
-        }*/
+        }
         if(!currentPlayer.isInJail())
             movePiece(diceSum);
 
     }
 
+    /* Image To Int
+    * gets the toString node of the image currently displayed as rolled
+    * on the die of the stack pane given,
+    * then splits the string wherever there is an underscore
+    * then a switch statement on the index 2
+    * with a substring from the start to wherever
+    * the first word ends by setting the end of the substring to
+    * a comma then returns the value of which word is listed
+    * this switch statement defaults to 1 if there is an unknown value
+    */
     private int imageToInt(StackPane stack)
     {
         String result = stack.getChildren().get(0).toString();
@@ -370,16 +452,22 @@ public class GamePlayController implements Initializable
 
         switch (imageViewSplit[2].substring(0, imageViewSplit[2].indexOf(",")))
         {
-            case "one" -> { return 1; }
             case "two" -> { return 2; }
             case "three" ->  { return 3; }
             case "four" ->  { return 4; }
             case "five" ->  { return 5; }
             case "six" ->  { return 6; }
-            default ->  { return 7; }
+            default ->  { return 1; }
         }
     }
 
+    /* Rolling Frame
+    * this is the method that makes the dice roll animation
+    * it does this by setting the current die not be visible
+    * then removing the die from the stack panes
+    * and adding a new die image selected by a random number
+    * and sets the visibility values of the new die to true
+    */
     private void rollingFrame()
     {
         left_stack.getChildren().get(0).setVisible(false);
@@ -396,8 +484,14 @@ public class GamePlayController implements Initializable
 
     }
 
+    // Rand Die Num: returns a random number from 1 to 6
     private static int randDieNum()  { return (int) (Math.random() * (6 - 1) * 1); }
 
+    /* Set Die Lists
+    * this method sets the arrays for the left and right die
+    * each array is initialized to have an imageview in each index,
+    * all of which has the die reflect the index in which it is placed
+    */
     private void setDieLists()
     {
         rightDie = new ImageView[]{right_die_one, right_die_two, right_die_three, right_die_four,
@@ -410,6 +504,21 @@ public class GamePlayController implements Initializable
 //endregion
 
     //region actions after roll
+
+    /* Move Piece
+    * takes an int value for how many spaces the player will move
+    * then calculates the where the player will land
+    * removing the current player pieces from the cell it is on
+    * if the end pane has any child object
+    * an arraylist will be created to get all the player
+    * pieces on the end pane cell
+    * then sets an id to each of the cells
+    * the dynamic player pieces method is called passing in
+    * the player piece width, the arraylist that was just made
+    * piecesOnEndPane, and the pane the cell is parallel to
+    * and places the player Piece to where it is going
+    * then calls the landingLocationFunctions method
+    */
     private void movePiece(int amountToMove)
     {
         Rectangle playerPiece = currentPlayer.getPlayerPiece();
@@ -426,28 +535,28 @@ public class GamePlayController implements Initializable
         endPane = paneList[currentPlayer.getPosition()];
         if(!endPane.getChildren().isEmpty())
         {
-            ArrayList<Rectangle> fortnite = new ArrayList<>();
+            ArrayList<Rectangle> piecesOnEndPane = new ArrayList<>();
 
             for (Node n : endPane.getChildren())
             {
                 if (n instanceof Rectangle)
                 {
-                    fortnite.add((Rectangle) n);
+                    piecesOnEndPane.add((Rectangle) n);
                 }
             }
 
-            for (int i = 0; i < fortnite.size(); i++)
-            {
-                fortnite.get(i).setId("piece" + i);
-            }
-
-            dynamicPlayPieces(playerPiece.getWidth(), fortnite, endPane);
+            dynamicPlayerPieces(playerPiece.getWidth(), piecesOnEndPane, endPane);
         }
 
         endPane.getChildren().add(playerPiece);
         landingLocationFunctions();
     }
 
+    /* Landing Location Functions
+    * this method takes the cell that the current player is on
+    * and checks the cell type, depending on the type of cell the player is on
+    * a different method is called
+    */
     private void landingLocationFunctions()
     {
         Cell cell = cellList.get(currentPlayer.getPosition());
@@ -471,6 +580,11 @@ public class GamePlayController implements Initializable
         updateAllText();
     }
 
+    /* Update All Text
+    * changes all the text to be current
+    * such as when users change or
+    * when money is spent
+    */
     private void updateAllText()
     {
         current_player_info_LBL.setText(currentPlayer.getName() + " has $" + currentPlayer.getBalance());
@@ -484,10 +598,18 @@ public class GamePlayController implements Initializable
         setPropertiesTable();
     }
 
+    /* Landing Space Info
+    * this method takes the cell that the player is going to land on
+    * then takes the name of the cell and puts it in a String
+    * then checks if the cell is owned by a player which will change the text
+    * to show who owns the cell
+    * then returns the string value
+    */
     private String landSpaceInfo(Cell currLoc)
     {
         String textForLanding = "You have landed on:\n"+ currLoc.getName();
-        boolean isPropertyType = currLoc.isOwned && (currLoc.getClass().getSimpleName()).equalsIgnoreCase("property");
+        boolean isPropertyType = currLoc.isOwned && (currLoc.getClass().getSimpleName())
+                                .equalsIgnoreCase("property");
         if(currLoc.isOwned() && isPropertyType)
         {
             textForLanding += "\nThis space is owned by:\n" + currLoc.getOwnerName()
@@ -496,8 +618,18 @@ public class GamePlayController implements Initializable
         return textForLanding;
     }
 
+    /* Go To Jail
+    * sends the player to jail
+    * setting the play's boolean value of inJail true
+    * and setting the doubles boolean to false
+    * so the player can no longer move
+    * then moves the player piece to visually be in the jail pane
+    * while setting the player to have their position set to 10,
+    * which is the index of just visiting jail,
+    */
     private void goToJail()
     {
+        doubles = false;
         currentPlayer.setInJail(true);
         Rectangle playerPiece = currentPlayer.getPlayerPiece();
 
@@ -534,6 +666,11 @@ public class GamePlayController implements Initializable
 
     }
 
+    /* Pay Rent
+    * checks the cost of the cell and
+    * takes money equal to the cost from the current players' balance
+    * and adds it to the owner of the property's balance
+    */
     private void payRent(Cell cell)
     {
         int amountPaid = cell.getCost();
@@ -542,6 +679,19 @@ public class GamePlayController implements Initializable
         updateGameLog(currentPlayer.getName(), cell.getName(), "pay-rent");
     }
 
+    /* Purchase
+    * gets the cell the player is on,
+    * if the player has not moved then the game will send an alert
+    * saying you can only buy properties after you move,
+    * then checks if the selected cell is owned
+    * makes and alert saying you cannot buy someone else's properties
+    * if the cell is a property type and is not owned
+    * and if the player can afford the cell
+    * the cost is removed from the player's balance
+    * and sets the cell to be owned and adds the cell to be
+    * part of the player's own properties
+    * then calls updateAllText
+    */
     public void purchase(ActionEvent ignoredE)
     {
         Cell cellToBuy = cellList.get(currentPlayer.getPosition());
@@ -566,12 +716,15 @@ public class GamePlayController implements Initializable
                 currentPlayer.setBalance(currentPlayer.getBalance()-cellToBuy.getCost());
                 cellToBuy.setOwned(true, currentPlayer);
                 currentPlayer.buyProperty(cellToBuy);
-                System.out.println("Successful purchase");
+
                 updateGameLog(currentPlayer.getName(), cellToBuy.getName(), "buy-prop");
                 updateAllText();
             }
         }
     }
+    /* Set Properties Table
+    * adds the properties owned by the current player to a table view
+    **/
     private void setPropertiesTable()
     {
         ObservableList<Cell> listData;
@@ -757,9 +910,8 @@ public class GamePlayController implements Initializable
     }
 
     //endregion
-
-
-
-
 }
+
+
+
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.monopoly_tm {
     requires javafx.fxml;
     requires json.simple;
     requires java.sql;
+    requires java.desktop;
 
 
     opens com.example.monopoly_tm to javafx.fxml;
